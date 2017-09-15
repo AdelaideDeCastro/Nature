627be39ae17e3ebddbd8899d2b441fc5466c4b4c
AdeBia <decastroadelaide@gmail.com>
2017-09-15 20:08:12 +0200

    calendar

--- TypeScriptHTMLApp2/js/app.js
+++ TypeScriptHTMLApp2/js/app.js
@@ -1,11 +1,31 @@
 /// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="../scripts/typings/jqueryui/jqueryui.d.ts" />
+///// <reference path="../scripts/typings/require/require.d.ts" />
+//require.config({
+//    baseUrl: 'js',
+//    //except, if the module ID starts with "app",
+//    //load it from the js/app directory. paths
+//    //config is relative to the baseUrl, and
+//    //never includes a ".js" extension since
+//    //the paths config could be for a directory.
+//    paths: {
+//        jquery: 'jquery',
+//        jqueryUI: 'jquery-ui',
+//        app:'app'
+//    },
+//    shim: {
+//        app: {
+//            deps: ['jquery', 'jqueryUI']
+//        },
+//        jqueryUI: {
+//            deps: ['jquery']
+//        }
+//    }
+//})
 window.onload = function () {
     var i = 0;
     showImage(i);
     checkDateIn();
-    //checkDateOut();
-    checkHorseRide();
-    //horseRide();
 };
 function showImage(i) {
     var images = $('img.showMe').length;
@@ -33,63 +53,108 @@ function showImage(i) {
     });
 }
 function checkDateIn() {
+    var today = new Date();
+    var tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000 * 365);
     $('#datepicker').datepicker({
         showAnim: "fold",
         showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+        buttonImage: "image/logoCalendar.png",
         buttonImageOnly: true,
         showButtonPanel: true,
         showOtherMonths: true,
-        dateRange: true,
         stepMonths: 1,
+        minDate: new Date,
+        maxDate: tomorrow,
+        onSelect: function (dateText) {
+            var sD = new Date(dateText);
+            $('#dateRangepicker').datepicker('option', 'minDate', sD);
+        }
     });
     $('#dateRangepicker').datepicker({
         showAnim: "fold",
         showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+        buttonImage: "image/logoCalendar.png",
         buttonImageOnly: true,
         showButtonPanel: true,
         showOtherMonths: true,
-        dateRange: true,
-        stepMonths: 1
+        stepMonths: 1,
+        minDate: new Date,
+        maxDate: tomorrow,
+        onSelect: function (dateText) {
+            var sD = new Date(dateText);
+            $('#datepicker').datepicker('option', 'maxDate', sD);
+        }
+    });
+    var tomorrow2 = new Date(today.getTime() + 24 * 60 * 60 * 1000 * 182.5);
+    $('#datepickerHorse').datepicker({
+        showAnim: "fold",
+        showOn: "button",
+        buttonImage: "image/logoCalendar.png",
+        buttonImageOnly: true,
+        showButtonPanel: true,
+        showOtherMonths: true,
+        stepMonths: 1,
+        minDate: new Date,
+        maxDate: tomorrow2,
+        onSelect: function (dateText) {
+            var sD = new Date(dateText);
+            $('#dateRangepicker').datepicker('option', 'maxDate', sD);
+        }
+    });
+    $('#submitDay').click(function () {
+        $('#horse1').css('display', 'block');
+        $('.checkAge').click(function (evt) {
+            if (evt.currentTarget.value == "0-6") {
+                $('#pony.showHorse').css("display", "block");
+            }
+            else if (evt.currentTarget.value == "7-15") {
+                $('#medio.showHorse').css("display", "block");
+            }
+            else {
+                $('#grande.showHorse').css("display", "block");
+            }
+        });
     });
 }
-//function checkDateOut() {
-//    $('#dateRangepicker').datepicker({
+//function checkHorseRide() {
+//    $('#datepickerHorse').datepicker({
 //        showAnim: "fold",
 //        showOn: "button",
 //        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
 //        buttonImageOnly: true,
 //        showButtonPanel: true,
 //        showOtherMonths: true,
-//        dateRange: true,
 //        stepMonths: 1
-//    })
+//    }
+//    )
 //}
-function checkHorseRide() {
-    $('#datepickerHorse').datepicker({
-        showAnim: "fold",
-        showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
-        buttonImageOnly: true,
-        showButtonPanel: true,
-        showOtherMonths: true,
-        dateRange: true,
-        stepMonths: 1
-    });
-}
-function horseRide() {
-    var age;
-    $('.checkAge').click(function (evt) {
-        if (evt.currentTarget.value == "0-6") {
-            $('#pony.showHorse').css("display", "block");
-        }
-        else if (evt.currentTarget.value == "7-15") {
-            $('#medio.showHorse').css("display", "block");
-        }
-        else {
-            $('#grande.showHorse').css("display", "block");
-        }
-    });
-}
+//function horseRide() {
+//    let age;
+//    $('checkHorseRide').click(function () {
+//        $(this).datepicker({
+//            showAnim: "fold",
+//            showOn: "button",
+//            buttonImageOnly: true,
+//            showButtonPanel: true,
+//            showOtherMonths: true,
+//            stepMonths: 1
+//        })
+//        $('#submitDay').click(function () {
+//            $('#horse1').css('display', 'block')
+//        }
+//        )
+//    })
+//    $('.checkAge').click(function (evt) {
+//        if ((evt.currentTarget as any).value == "0-6") {
+//            $('#pony.showHorse').css("display", "block");
+//        }
+//        else if ((evt.currentTarget as any).value == "7-15") {
+//            $('#medio.showHorse').css("display", "block");
+//        }
+//        else {
+//            $('#grande.showHorse').css("display", "block");
+//        }
+//    }
+//    )
+//} 
 //# sourceMappingURL=app.js.map
--- TypeScriptHTMLApp2/js/app.ts
+++ TypeScriptHTMLApp2/js/app.ts
@@ -1,14 +1,38 @@
 ﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="../scripts/typings/jqueryui/jqueryui.d.ts" />
 
+
+///// <reference path="../scripts/typings/require/require.d.ts" />
+
+//require.config({
+//    baseUrl: 'js',
+//    //except, if the module ID starts with "app",
+//    //load it from the js/app directory. paths
+//    //config is relative to the baseUrl, and
+//    //never includes a ".js" extension since
+//    //the paths config could be for a directory.
+//    paths: {
+//        jquery: 'jquery',
+//        jqueryUI: 'jquery-ui',
+//        app:'app'
+//    },
+//    shim: {
+//        app: {
+//            deps: ['jquery', 'jqueryUI']
+//        },
+//        jqueryUI: {
+//            deps: ['jquery']
+//        }
+//    }
+
+//})
 
 
 window.onload = function () {
     let i = 0;
     showImage(i);
     checkDateIn();
-    //checkDateOut();
-    checkHorseRide();
-    //horseRide();
+
 
 }
 
@@ -18,7 +42,7 @@ function showImage(i) {
         if (i == 0) {
             $('#arrowLeft').css('display', 'block');
         }
-        if (i == images-2) {
+        if (i == images - 2) {
             $('#arrowRight').css('display', 'none');
         }
         $('img.showMe').eq(i).css('display', 'none');
@@ -27,7 +51,7 @@ function showImage(i) {
     }
     )
     $('#arrowLeft').click(function () {
- 
+
         if (i == 1) {
             $('#arrowLeft').css('display', 'none');
         }
@@ -35,71 +59,122 @@ function showImage(i) {
             $('#arrowRight').css('display', 'block');
         }
         $('img.showMe').eq(i).css('display', 'none');
-        $('img.showMe').eq(i -1).css('display', 'block');
+        $('img.showMe').eq(i - 1).css('display', 'block');
         i--;
     }
     )
 }
+
 function checkDateIn() {
+    let today = new Date();
+    let tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000 * 365);
+
     $('#datepicker').datepicker({
         showAnim: "fold",
         showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+        buttonImage: "image/logoCalendar.png",
         buttonImageOnly: true,
         showButtonPanel: true,
         showOtherMonths: true,
-        dateRange: true,
         stepMonths: 1,
-
+        minDate: new Date,
+        maxDate: tomorrow,
+        onSelect: function (dateText) {
+            let sD = new Date(dateText);
+            $('#dateRangepicker').datepicker('option', 'minDate', sD);
+        }
     })
+
     $('#dateRangepicker').datepicker({
         showAnim: "fold",
         showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+        buttonImage: "image/logoCalendar.png",
         buttonImageOnly: true,
         showButtonPanel: true,
         showOtherMonths: true,
-        dateRange: true,
-        stepMonths: 1
+        stepMonths: 1,
+        minDate: new Date,
+        maxDate: tomorrow,
+        onSelect: function (dateText) {
+            let sD = new Date(dateText);
+            $('#datepicker').datepicker('option', 'maxDate', sD);
+        }
     })
+
+        let tomorrow2 = new Date(today.getTime() + 24 * 60 * 60 * 1000 * 182.5);
+        $('#datepickerHorse').datepicker({
+            showAnim: "fold",
+            showOn: "button",
+            buttonImage: "image/logoCalendar.png",
+            buttonImageOnly: true,
+            showButtonPanel: true,
+            showOtherMonths: true,
+            stepMonths: 1,
+            minDate: new Date,
+            maxDate: tomorrow2,
+            onSelect: function (dateText) {
+                let sD = new Date(dateText);
+                $('#dateRangepicker').datepicker('option', 'maxDate', sD);
+            }
+        })
+    $('#submitDay').click(function () {
+        $('#horse1').css('display', 'block');
+
+        $('.checkAge').click(function (evt) {
+            if ((evt.currentTarget as any).value == "0-6") {
+                $('#pony.showHorse').css("display", "block");
+            }
+            else if ((evt.currentTarget as any).value == "7-15") {
+                $('#medio.showHorse').css("display", "block");
+            }
+            else {
+                $('#grande.showHorse').css("display", "block");
+            }
+        }
+        )
+    }
+    )
 }
-//function checkDateOut() {
-//    $('#dateRangepicker').datepicker({
+
+//function checkHorseRide() {
+//    $('#datepickerHorse').datepicker({
 //        showAnim: "fold",
 //        showOn: "button",
 //        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
 //        buttonImageOnly: true,
 //        showButtonPanel: true,
 //        showOtherMonths: true,
-//        dateRange: true,
 //        stepMonths: 1
-//    })
+//    }
+//    )
 //}
-function checkHorseRide() {
-    $('#datepickerHorse').datepicker({
-        showAnim: "fold",
-        showOn: "button",
-        buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
-        buttonImageOnly: true,
-        showButtonPanel: true,
-        showOtherMonths: true,
-        dateRange: true,
-        stepMonths: 1
-    }
-        )
-        }
-function horseRide() {
-            let age;
-            $('.checkAge').click(function (evt) {
-                if ((evt.currentTarget as any).value == "0-6") {
-                    $('#pony.showHorse').css("display", "block");
-                }
-                else if ((evt.currentTarget as any).value == "7-15") {
-                    $('#medio.showHorse').css("display", "block");
-                }
-                else {
-                    $('#grande.showHorse').css("display", "block");
-                }
-            }
-            )
-        }
+
+//function horseRide() {
+//    let age;
+//    $('checkHorseRide').click(function () {
+//        $(this).datepicker({
+//            showAnim: "fold",
+//            showOn: "button",
+//            buttonImageOnly: true,
+//            showButtonPanel: true,
+//            showOtherMonths: true,
+//            stepMonths: 1
+//        })
+//        $('#submitDay').click(function () {
+//            $('#horse1').css('display', 'block')
+//        }
+//        )
+//    })
+//    $('.checkAge').click(function (evt) {
+//        if ((evt.currentTarget as any).value == "0-6") {
+//            $('#pony.showHorse').css("display", "block");
+//        }
+//        else if ((evt.currentTarget as any).value == "7-15") {
+//            $('#medio.showHorse').css("display", "block");
+//        }
+//        else {
+//            $('#grande.showHorse').css("display", "block");
+//        }
+//    }
+//    )
+//}
